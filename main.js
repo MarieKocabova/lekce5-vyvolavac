857abe36bc8e0b894d5761ee9da61c832e0bb4a1
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-04-06 21:30:47 +0200

    doplněn bonusový úkol

--- main.js
+++ main.js
@@ -35,27 +35,43 @@ function tahnoutJmeno() {
     }
 
     // Generujeme náhodný index
-
     let vyherniIndex = Math.floor(Math.random()* jmena.length);
     
-
     // Získáme výherní jméno na patřičném indexu
     let vyherniJmeno = jmena[vyherniIndex];
     
-
     // Vyřadíme vylosované jméno z osudí
     jmena.splice(vyherniIndex, 1);
     
-
     // Výherní jméno si uložíme do pole k ostatním výherním
-    tazenaJmena.push(vyherniJmeno);
-     
+    //tazenaJmena.push(vyherniJmeno);
 
+    //přidej vyvolané jméno na začátek
+    tazenaJmena.unshift(vyherniJmeno);
+
+    //přidej počítadlo, kolik se vybralo holek
+    let vyvolanoBylo = tazenaJmena.length
+    let oznaceni;
     
+    if(vyvolanoBylo == 1) {
+        oznaceni = " holka"
+    } else if(vyvolanoBylo < 5) {
+        oznaceni = " holky"
+    } else {
+        oznaceni = " holek"
+    }
+       
 
     let vyherce = document.querySelector('#vyherka');
     vyherce.innerHTML = vyherniJmeno;
 
     let seznamJmen = document.querySelector('#seznam');
     seznamJmen.innerHTML = tazenaJmena + ", "
+
+    let pocetVybranych = document.querySelector('#pocet');
+    pocetVybranych.innerHTML = vyvolanoBylo + oznaceni
+}
+
+function restart() {
+    window.location.reload(); 
 }
